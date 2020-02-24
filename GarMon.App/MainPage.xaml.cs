fcfd21adc21fe7086e3d3a22fd64416ac429cfb4
Jared Buchanan <jbbuchanan266@gmail.com>
2020-02-24 10:15:24 -0700

    initial commit

---
+++ GarMon.App/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Net.Security;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GarMon.App
+{
+    public sealed partial class MainPage : Page
+    {
+        int ticks = 0;
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SendEmail();
+        }
+
+        private void SendEmail()
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Joey Tribbiani", "jbbuchanan266@gmail.com"));
+            message.To.Add(new MailboxAddress("Mrs. Chanandler Bong", "jbbuchanan266@gmail.com"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Hey Chandler,I just wanted to let you know that Monica and I were going to go play some paintball, you in?-- Joey"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587);
+
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("YOUR_GMAIL_NAME", "YOUR_PASSWORD");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
